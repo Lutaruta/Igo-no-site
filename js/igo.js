c3e2b4e7a714e77578cb67a2461a3bf7f177113c
Lutaruta <t125039@ed.sus.ac.jp>
2025-08-01 19:42:20 +0900

    Add second websites files

---
+++ js/igo.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleButton = document.getElementById('joseki-toggle-button');
+  const listContainer = document.getElementById('joseki-list-container');
+
+  //ボタンがnullでないことを確認してからイベントリスナーを設定
+  if (toggleButton && listContainer) {
+
+    //ボタンにクリックイベントを設定
+    toggleButton.addEventListener('click', function() {
+
+      //classList.toggle() を使って 'is-hidden' クラスを付け外しする
+      listContainer.classList.toggle('is-hidden');
+
+    });
+  }
+
+});
+  const heroSection = document.getElementById('hero-section');
+  const toggleButton = document.querySelector('.toggle-button');
+
+  // クリックしたときに動かす
+  toggleButton.addEventListener('click', () => {
+    heroSection.classList.toggle('is-collapsed');
+  });
+
+  //定石カードの表示
+const josekiData = [
+  {
+    name: '三三の定石',
+    image: 'img/三三.png',
+    link: 'sansan.html'
+  },
+  {
+    name: '桂馬の定石',
+    image: 'img/桂馬.png',
+    link: 'keima.html'
+  },
+];
+const josekiListContainer = document.getElementById('top-joseki-list');
+josekiData.forEach(joseki => {
+  const listItem = `
+    <li>
+      <a href="${joseki.link}" class="move-card-link">
+        <div class="move-card">
+          <img src="${joseki.image}" alt="${joseki.name}">
+          <p class="name">${joseki.name}</p>
+        </div>
+      </a>
+    </li>
+  `;
+  josekiListContainer.insertAdjacentHTML('beforeend', listItem);
+});
